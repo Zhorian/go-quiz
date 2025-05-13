3604c38037db252f4bf57b6020bc556c247138c4
Gareth (Linux) <garethkear@outlook.com>
2025-05-13 20:30:11 +0100

    created a gamestate struct

--- main.go
+++ main.go
@@ -12,16 +12,43 @@ import (
 	"time"
 )
 
+type problem struct {
+	question string
+	answer   string
+}
+
 type config struct {
-	questionFilePath   string
-	shuffleQuestions   bool
+	problemFilePath    string
+	shuffleProblems    bool
 	timeLimitInSeconds int
 }
 
-func loadQuestions(config config) ([][]string, error) {
-	fmt.Println("Loading questions from:", config.questionFilePath)
+type gameState struct {
+	problems []problem
+	score    int
+	done     chan bool
+}
 
-	file, err := os.Open(config.questionFilePath)
+func parseProblems(rawProblems [][]string) ([]problem, error) {
+	var problems []problem
+	for _, rawProblem := range rawProblems {
+		if len(rawProblem) < 2 {
+			return nil, fmt.Errorf("invalid question format: %v", rawProblem)
+		}
+
+		q := problem{
+			question: strings.TrimSpace(rawProblem[0]),
+			answer:   strings.TrimSpace(rawProblem[1]),
+		}
+		problems = append(problems, q)
+	}
+	return problems, nil
+}
+
+func loadProblems(config config) ([]problem, error) {
+	fmt.Println("Loading problems from:", config.problemFilePath)
+
+	file, err := os.Open(config.problemFilePath)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return nil, err
@@ -29,32 +56,38 @@ func loadQuestions(config config) ([][]string, error) {
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-	questions, err := reader.ReadAll()
+	rawQuestions, err := reader.ReadAll()
 	if err != nil {
 		fmt.Println("Error reading CSV file:", err)
 		return nil, err
 	}
 
-	if !config.shuffleQuestions {
-		return questions, nil
+	problem, err := parseProblems(rawQuestions)
+	if err != nil {
+		fmt.Println("Error parsing problems:", err)
+		return nil, err
 	}
 
-	rand.Shuffle(len(questions), func(i, j int) {
-		questions[i], questions[j] = questions[j], questions[i]
+	if !config.shuffleProblems {
+		return problem, nil
+	}
+
+	rand.Shuffle(len(problem), func(i, j int) {
+		problem[i], problem[j] = problem[j], problem[i]
 	})
 
-	return questions, nil
+	return problem, nil
 }
 
-func askQuestion(questionNumber int, totalQuestions int, question []string) (bool, error) {
-	correctAnswer := question[1]
+func askQuestion(questionNumber int, totalQuestions int, problem problem) (bool, error) {
+	correctAnswer := problem.answer
 	correctAnswerAsInt, err := strconv.Atoi(strings.TrimSpace(correctAnswer))
 	if err != nil {
 		fmt.Println("Error converting correct answer to int:", err)
 		return false, err
 	}
 
-	fmt.Printf("Question %d of %d: %s\n", questionNumber, totalQuestions, question[0])
+	fmt.Printf("Question %d of %d: %s\n", questionNumber, totalQuestions, problem.question)
 	var answer string
 	fmt.Scanln(&answer)
 	answerAsInt, err := strconv.Atoi(answer)
@@ -67,20 +100,36 @@ func askQuestion(questionNumber int, totalQuestions int, question []string) (boo
 		fmt.Println("Correct!")
 		return true, nil
 	} else {
-		fmt.Println("Incorrect! The correct answer is:", question[1])
+		fmt.Println("Incorrect! The correct answer is:", problem.answer)
 		return false, nil
 	}
 }
 
+func runQuiz(gameState gameState) {
+	for i, question := range gameState.problems {
+		result, err := askQuestion(i+1, len(gameState.problems), question)
+		if err != nil {
+			fmt.Println("Error asking question:", err)
+			return
+		}
+
+		if result {
+			gameState.score++
+		}
+	}
+
+	gameState.done <- true
+}
+
 func main() {
 	var config config
-	flag.StringVar(&config.questionFilePath, "file", "./assets/default-questions.csv", "Path to the CSV file containing questions")
-	flag.BoolVar(&config.shuffleQuestions, "shuffle", false, "Shuffle questions before asking")
+	flag.StringVar(&config.problemFilePath, "file", "./assets/default-questions.csv", "Path to the CSV file containing questions")
+	flag.BoolVar(&config.shuffleProblems, "shuffle", false, "Shuffle questions before asking")
 	flag.IntVar(&config.timeLimitInSeconds, "time", 30, "Time limit for each question in seconds (0 for no limit)")
 	flag.Parse()
 
 	println("Welcome to Math Quiz!")
-	questions, err := loadQuestions(config)
+	problems, err := loadProblems(config)
 	if err != nil {
 		fmt.Println("Error loading questions:", err)
 		return
@@ -97,32 +146,19 @@ func main() {
 		defer cancel()
 	}
 
-	numberOfQuestions := len(questions)
-	totalScore := 0
+	gameState := gameState{
+		problems: problems,
+		score:    0,
+		done:     make(chan bool),
+	}
 
-	done := make(chan bool)
-	go func() {
-		for i, question := range questions {
-			result, err := askQuestion(i+1, numberOfQuestions, question)
-			if err != nil {
-				fmt.Println("Error asking question:", err)
-				return
-			}
-
-			if result {
-				totalScore++
-			}
-		}
-
-		done <- true
-
-	}()
+	go func() { runQuiz(gameState) }()
 
 	select {
-	case <-done:
-		fmt.Printf("Quiz complete! You answered %d out of %d questions correctly.\n!", totalScore, numberOfQuestions)
+	case <-gameState.done:
+		fmt.Printf("Quiz complete! You answered %d out of %d questions correctly.\n!", gameState.score, len(gameState.problems))
 	case <-ctx.Done():
-		fmt.Println("Time's up! You answered", totalScore, "out of", numberOfQuestions, "questions correctly.")
+		fmt.Println("Time's up! You answered", gameState.score, "out of", gameState.score, len(gameState.problems), "questions correctly.")
 		fmt.Println("Exiting the quiz.")
 	}
 }
